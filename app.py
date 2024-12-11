89de96bfbb1733045c02f46a8df5c10546c78c8f
Maris <maris707@inbox.lv>
2024-12-11 12:54:08 +0200

    11.12.2024

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def add_data():
     return jsonify({"message": "Dati saglabāti veiksmīgi!"}),201
 
 
-__name__ == "__main__":
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
